90eb35d23d8d463cc97f6bcd5647c4caa79d4e00
PrkRajput <prajput@ITEM-S66888.dhcp.noid.in.sopra>
2019-06-20 15:05:44 +0530

    LL updated

--- src/linkedList/LinkedList.java
+++ src/linkedList/LinkedList.java
@@ -3,7 +3,7 @@ package linkedList;
 public class LinkedList {
 	Node head = null;
 	int size=0;
-/////////////////// INSERT AT END	//////////////////////////////////////////////
+///////////////////////////// INSERT AT END	////////////////////////////////////////////
 	public LinkedList insertAtEnd(LinkedList list, int data) {
 		Node node = new Node(data);
 		if (list.head == null) {
@@ -18,11 +18,12 @@ public class LinkedList {
 		return list;
 	}
 
-//////////////////////// DISPLAY THE LIST// ////////////////////////////////////////
+//////////////////////// DISPLAY THE LIST ///////////////////////////////////////////
 	public void showList(LinkedList list) {
 		if (list.head == null) {
 			System.out.println("List is empty");
 		} else {
+			System.out.println("");
 			Node temp = list.head;
 			while (temp.next != null) {
 				System.out.print(temp.data + " -> ");
@@ -32,7 +33,7 @@ public class LinkedList {
 		}
 	}
 
-//////////////////////// INSERT AT BEGINING // /////////////////////////////////////////
+//////////////////////// INSERT AT BEGINING ////////////////////////////////////////////
 	public LinkedList insertAtBegining(LinkedList list, int data) {
 
 		if (list.head == null) {
@@ -67,5 +68,50 @@ public class LinkedList {
 				temp.next = newNode;
 			}
 		return list;
+	}
+/////////////////////// DELETE A NODE AT INDEX ///////////////////////////////////////
+	public LinkedList deleteAtIndex (LinkedList list, int index){
+		if (list.head == null) {
+			System.out.println("List is empty");
+		} else {
+			Node temp = list.head;
+			for(int i=1; i<index-1; i++) {
+				temp = temp.next;
+			}
+			System.out.println("\nDeleted : "+temp.next.data);
+			temp.next = temp.next.next;		
+		}
+		size--;
+		return list;
+	}
+	
+	
+///////////////////////////////  SWAP ADJACENT ///////////////////////////////////////
+	public LinkedList swapAdjacent(LinkedList list){
+		LinkedList tempList = new LinkedList();
+		
+		Node tempHead = list.head;
+		while(tempHead!=null && tempHead.next!=null){
+			tempList.insertAtEnd(tempList, tempHead.next.data);
+			tempList.insertAtEnd(tempList, tempHead.data);
+			if(tempHead.next.next!=null)
+				tempHead=tempHead.next.next;
+			else
+				break;			
+		}
+		if(tempHead!=null)
+			tempList.insertAtEnd(tempList, tempHead.data);
+		
+		return tempList;
+	}
+////////////////////////////////// GET MIDDLE OF LIST ///////////////////////////////////////	
+	public int getMiddleOfList(LinkedList list){
+			Node node1 = list.head;
+			Node node2 = list.head;
+			while(node2!=null && node2.next!=null){
+				node1=node1.next;
+				node2=node2.next.next;
+			}		
+		return node1.data;
 	}
 }
